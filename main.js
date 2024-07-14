d90600aec6217cb6b43dbded91ef3d5d14f65a34
gerrycoleman <webdevcole@icloud.com>
2024-07-15 00:21:15 +0300

    update

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+document.addEventListener('DOMContentLoaded', function() {
+ const menuOpen = document.querySelector('.menu-open');
+ const menuClose = document.querySelector('.menu-close');
+ const navUl = document.querySelector('nav ul');
+
+ menuOpen.addEventListener('click', function() {
+  navUl.classList.add('open');
+ });
+
+ menuClose.addEventListener('click', function() {
+  navUl.classList.remove('open');
+ });
+});
